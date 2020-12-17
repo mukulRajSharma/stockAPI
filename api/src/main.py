d9e0c455423058747765af628da36b66c4483130
jerry8tom <46439599+jerry8tom@users.noreply.github.com>
2020-12-17 19:54:46 +1100

    setup directory

---
+++ api/src/main.py
@@ -0,0 +1,32 @@
+import flask
+from flask import jsonify, request
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
+
+
+@app.route('/api', methods=['POST', 'GET'])
+def ships():
+    return "<a href='/api/stocks'>Click me</a>"
+
+
+@app.route('/api/stocks', methods=['GET'])
+def api_id():
+
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    # Create an empty list for our results
+    results = []
+
+    return jsonify(results)
+
+
+app.run()
